b59f3a250db5650d8c21ddf417f3fd3988bf85c1
Старостин Алексей <starostin.lexa2011@yandex.ru>
2022-08-18 19:01:03 +0300

    "update"

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 from pprint import pprint
-
+import os
 book = {}
 catalog = 'catalog.txt'
 def cat_log(catalog):
     with open(catalog, encoding='utf-8') as file_s:
-
         for file in file_s:
             name_of_dish = file.strip()
             good = []
@@ -27,20 +26,48 @@ def get_shop_list_by_dishes(dishes, person_list):
             for ingridients in book[dish]:
                 ing = int(ingridients['quantity'])
                 name = ingridients['ingredient_name']
-
                 if ingridients['ingredient_name'] in resurs:
                     resurs[name]['quantity'] = ing * person_list
                 else:
                     resurs[name] = {'measure': ingridients['measure'],
-                                                'quantity' : ing * person_list }
-
-
+                                                'quantity' : ing * person_list}
     return resurs
-
-
-
 recipe = get_shop_list_by_dishes(['Фахитос', 'Омлет'], 2)
 pprint(recipe)
+print()
 
+def len_story(*txt_file):
+    res = {}
+    for file in txt_file:
+        with open(file, encoding='utf-8') as files:
+            text_len = len(files.readlines())
+            res.setdefault(file, text_len)
+    sort_txt = sorted(res, key=res.get)
+    sort_text = {}
+    for fi_le in sort_txt:
+        sort_text[fi_le] = res[fi_le]
+    return sort_text
+
+
+def story_t(dictionary):
+    base_path = os.getcwd()
+    story_text = 'story.txt'
+
+    full_path = os.path.join(base_path, story_text)
+    text = {}
+    for k, v in dictionary.items():
+        with open(k, encoding='utf-8') as file:
+            text.setdefault(k, file.read().strip())
+
+    with open(full_path, 'a', encoding='utf-8') as files:
+        for key, value in text.items():
+            files.write(f'{key}\n')
+            files.write(f'{str(dictionary[key])}\n')
+            files.write(f'{value}\n')
+
+
+new_file = len_story('text1.txt', 'text2.txt', 'text3.txt')
+
+story_t(new_file)
 
 
